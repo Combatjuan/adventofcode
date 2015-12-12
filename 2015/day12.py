d37998b0ac2ea761f96c3ac1a55fe990cfdc7744
combatjuan <charles.harwood@zuerchertech.com>
2015-12-12 15:26:40 -0600

    Day 12.  Thanks json library.

---
+++ 2015/day12.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import json
+import sys
+
+sum = 0
+with open(sys.argv[1], 'r') as f:
+	j = json.load(f)
+
+def just_values(value):
+	val_list = []
+	if isinstance(value, dict):
+		for k, v in value.items():
+			val_list.extend(just_values(v))
+	elif isinstance(value, list):
+		for v in value:
+			val_list.extend(just_values(v))
+	else:
+		val_list = [value]
+	return val_list
+
+d = dict(j)
+for v in just_values(d):
+	try:
+		sum = sum + int(v)
+	except:
+		pass
+
+print sum
+
