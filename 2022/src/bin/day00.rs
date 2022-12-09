7889ff04c4fc8912eca5dbe712fac733cda5a553
charwood <charwood@fastmail.com>
2022-12-08 21:56:05 -0600

    Simplify day00 for template use
    I tend to copy day00 when starting a new day.  This makes it more
    general and faster for me to get going as a new day.

--- 2022/src/bin/day00.rs
+++ 2022/src/bin/day00.rs
@@ -3,21 +3,10 @@ mod advent;
 use regex::Regex;
 use advent::{advent_exit, Calculator, LineParser, Solution};
 
-struct SumCalculator {}
-impl Calculator<i64, i64> for SumCalculator {
-	fn solve_a(&mut self, records: &Vec<i64>) -> Result<i64, String> {
-		let mut n : i64 = 0;
-		for r in records {
-			n += r;
-		}
-		Ok(n)
-	}
-	fn solve_b(&mut self, _records: &Vec<i64>) -> Result<i64, String> {
-		Err("Unimplemented".to_string())
-	}
-}
+type Answer = i64;
+type Record = i64;
 
-fn static_parse_line(line_number: usize, s: &String) -> Result<i64, String> {
+fn static_parse_line(line_number: usize, s: &String) -> Result<Record, String> {
 	let re = Regex::new(r"\d+").unwrap();
 	if let Some(cap) = re.captures(s) {
 		let number : i64 = cap.get(0).unwrap().as_str().parse::<i64>().unwrap();
@@ -27,9 +16,23 @@ fn static_parse_line(line_number: usize, s: &String) -> Result<i64, String> {
 	} 
 }
 
+struct SumCalculator {}
+impl Calculator<Record, Answer> for SumCalculator {
+	fn solve_a(&mut self, records: &Vec<Record>) -> Result<Answer, String> {
+		let mut n : i64 = 0;
+		for r in records {
+			n += r;
+		}
+		Ok(n)
+	}
+	fn solve_b(&mut self, _records: &Vec<Record>) -> Result<Answer, String> {
+		Err("Unimplemented".to_string())
+	}
+}
+
 fn main() {
 	let mut parser = LineParser::new(&static_parse_line);
 	let mut calculator = SumCalculator {};
-	let status = Solution::solve::<i64, i64>(&mut parser, &mut calculator);
+	let status = Solution::solve::<Record, Answer>(&mut parser, &mut calculator);
 	advent_exit(status);
 }
